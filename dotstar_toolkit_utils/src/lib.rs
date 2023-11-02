1dc9c25d69660e1b58c74268681b6ce56fe21be2
kriskras99 <admin@kriskras99.nl>
2023-11-02 16:41:11 +0000

    [.* Toolkit Utils] Use proper error types

--- dotstar_toolkit_utils/src/lib.rs
+++ dotstar_toolkit_utils/src/lib.rs
@@ -25,7 +25,7 @@
 //! 1. [`bytes`]: contains Byteorder like functions for reading integers and strings from byte slices.
 //! 2. [`testing`]: contains alternatives to the `assert!` family that return `Result`s instead of panicking.
 //! 3. [`vfs`]: contains traits for a virtual filesystem and some basic filesystems that allow for parsing without extracting
-//! 
+//!
 //! ## Features
 //! This crate has no features that can be enabled
 //!
--- dotstar_toolkit_utils/src/testing.rs
+++ dotstar_toolkit_utils/src/testing.rs
@@ -1,22 +1,89 @@
 //! # Testing
 //! Contains functions like `assert!` but they return an `Error` instead of panicking.
 
-use anyhow::{anyhow, Error};
 use std::fmt::Debug;
+use thiserror::Error;
+
+/// Errors returend when the test* functions fail
+#[derive(Error, Debug)]
+pub enum TestError {
+    /// The two values do not match
+    #[error("{left} does not match {right}")]
+    NotEqual {
+        /// Left value
+        left: String,
+        /// Right value
+        right: String,
+    },
+    /// The two values do match
+    #[error("{left} matches {right}")]
+    Equal {
+        /// Left value
+        left: String,
+        /// Right value
+        right: String,
+    },
+    /// The value is not any of the right values
+    #[error("{left} does not match any value in {right}")]
+    NotAny {
+        /// Left value
+        left: String,
+        /// Right value
+        right: String,
+    },
+    /// The value is greater than it's supposed to be
+    #[error("{left} is greater than {right}")]
+    GreaterThan {
+        /// Left value
+        left: String,
+        /// Right value
+        right: String,
+    },
+}
+
+impl TestError {
+    /// Create the [`TestError::NotEqual`] error
+    fn not_equal<T: Debug>(left: &T, right: &T) -> Self {
+        Self::NotEqual {
+            left: format!("{left:?}"),
+            right: format!("{right:?}"),
+        }
+    }
+
+    /// Create the [`TestError::Equal`] error
+    fn equal<T: Debug>(left: &T, right: &T) -> Self {
+        Self::Equal {
+            left: format!("{left:?}"),
+            right: format!("{right:?}"),
+        }
+    }
+
+    /// Create the [`TestError::NotAny`] error
+    fn not_any<T: Debug>(left: &T, right: &[T]) -> Self {
+        Self::NotAny {
+            left: format!("{left:?}"),
+            right: format!("{right:?}"),
+        }
+    }
+
+    /// Create the [`TestError::GreaterThan`] error
+    fn greater_than<T: Debug>(left: &T, right: &T) -> Self {
+        Self::GreaterThan {
+            left: format!("{left:?}"),
+            right: format!("{right:?}"),
+        }
+    }
+}
 
 /// Test if `one` == `two` returning a descriptive error if they're not the same.
 ///
 /// # Errors
 /// Will return an error if the two inputs are not the same, with a description of the values.
-///
-/// # Panics
-/// When compiled in debug mode, it will panic instead of returning an error.
-pub fn test<T: PartialEq + Debug>(one: &T, two: &T) -> Result<(), Error> {
-    // debug_assert!(one == two, "{one:?} does not match {two:?}!");
-    if one == two {
+pub fn test<T: PartialEq + Debug>(left: &T, right: &T) -> Result<(), TestError> {
+    if left == right {
         Ok(())
     } else {
-        Err(anyhow!("{one:?} does not match {two:?}!"))
+        Err(TestError::not_equal(left, right))
     }
 }
 
@@ -24,15 +91,11 @@ pub fn test<T: PartialEq + Debug>(one: &T, two: &T) -> Result<(), Error> {
 ///
 /// # Errors
 /// Will return an error if `two` does not contain `one`, with a description of the values.
-///
-/// # Panics
-/// When compiled in debug mode, it will panic instead of returning an error.
-pub fn test_any<T: PartialEq + Debug>(one: &T, two: &[T]) -> Result<(), Error> {
-    // debug_assert!(two.contains(one), "{one:?} does not match any value in {two:?}!");
-    if two.contains(one) {
+pub fn test_any<T: PartialEq + Debug>(left: &T, right: &[T]) -> Result<(), TestError> {
+    if right.contains(left) {
         Ok(())
     } else {
-        Err(anyhow!("{one:?} does not match any value in {two:?}!"))
+        Err(TestError::not_any(left, right))
     }
 }
 
@@ -40,16 +103,12 @@ pub fn test_any<T: PartialEq + Debug>(one: &T, two: &[T]) -> Result<(), Error> {
 ///
 /// # Errors
 /// Will return an error if the two inputs are the same, with a description of the values.
-///
-/// # Panics
-/// When compiled in debug mode, it will panic instead of returning an error.
 #[allow(clippy::if_not_else)] // Much clearer this way
-pub fn test_not<T: PartialEq + Debug>(one: &T, two: &T) -> Result<(), Error> {
-    debug_assert!(one != two, "{one:?} is the same as {two:?}!");
-    if one != two {
+pub fn test_not<T: PartialEq + Debug>(left: &T, right: &T) -> Result<(), TestError> {
+    if left != right {
         Ok(())
     } else {
-        Err(anyhow!("{one:?} is the same as {two:?}!"))
+        Err(TestError::equal(left, right))
     }
 }
 
@@ -57,14 +116,10 @@ pub fn test_not<T: PartialEq + Debug>(one: &T, two: &T) -> Result<(), Error> {
 ///
 /// # Errors
 /// Will return an error if `one` is bigger than `two`, with a description of the values.
-///
-/// # Panics
-/// When compiled in debug mode, it will panic instead of returning an error.
-pub fn test_le<T: PartialOrd + Debug>(one: &T, two: &T) -> Result<(), Error> {
-    debug_assert!(one <= two, "{one:?} is larger than {two:?}!");
-    if one <= two {
+pub fn test_le<T: PartialOrd + Debug>(left: &T, right: &T) -> Result<(), TestError> {
+    if left <= right {
         Ok(())
     } else {
-        Err(anyhow!("{one:?} is larger than {two:?}!"))
+        Err(TestError::greater_than(left, right))
     }
 }
