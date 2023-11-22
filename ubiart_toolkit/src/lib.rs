3824fed07e2b4a5c9616643aa9103ea292ed65a0
Christiaan Biesterbosch <Kriskras99@users.noreply.github.com>
2023-11-22 16:26:13 +0100

    [Generic] Move lints to workspace Cargo.toml

--- ubiart_toolkit/src/lib.rs
+++ ubiart_toolkit/src/lib.rs
@@ -1,22 +1,9 @@
-#![deny(clippy::all)]
-#![warn(clippy::pedantic)]
-#![warn(clippy::nursery)]
-#![warn(clippy::cargo)]
-// If an integer doesn't fit it's a good indication that something broke
-#![deny(clippy::as_conversions)]
-#![deny(clippy::empty_structs_with_brackets)]
-#![deny(clippy::get_unwrap)]
-#![deny(clippy::if_then_some_else_none)]
-#![deny(clippy::lossy_float_literal)]
-#![deny(clippy::missing_assert_message)]
 #![allow(clippy::too_many_lines)]
 // Doesn't work very well unk{n}
 #![allow(clippy::similar_names)]
 // #![allow(clippy::large_enum_variant)] // Broken
 #![allow(clippy::struct_excessive_bools)]
 #![allow(clippy::implicit_hasher)]
-// TODO: Remove when new version of Yoke is released
-#![allow(clippy::forget_non_drop)]
 // Broken by serde_with
 #![allow(clippy::multiple_crate_versions)]
 // Significantly less readable than the original
@@ -29,7 +16,7 @@
 
 //! # UbiArt Toolkit
 //! A library for parsing and writing various UbiArt files.
-//! 
+//!
 //! ## Features
 //! This crate has one feature that can be enabled:
 //! - `full_json_types`: Enable all JSON types (will increase compile times)
