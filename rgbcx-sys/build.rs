b0addb48589df9759e8f2f1cc3c5deac0da56ec3
kriskras99 <admin@kriskras99.nl>
2023-12-06 17:12:51 +0000

    [Rgbcx] Add alternative DXT encoder

---
+++ rgbcx-sys/build.rs
@@ -0,0 +1,91 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // This is the directory where the `c` library is located.
+    let libdir_path = PathBuf::from("rgbcx")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize path");
+
+    // This is the path to the `c` headers file.
+    let headers_path = libdir_path.join("rgbcx.hpp");
+    let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
+
+    // This is the path to the intermediate object file for our library.
+    let obj_path = libdir_path.join("rgbcx.o");
+    // This is the path to the static library file.
+    let lib_path = libdir_path.join("librgbcx.a");
+
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
+
+    // Tell cargo to tell rustc to link our `rgbcx` library. Cargo will
+    // automatically know it must look for a `librgbcx.a` file.
+    println!("cargo:rustc-link-lib=rgbcx");
+
+    // Tell cargo to invalidate the built crate whenever the header changes.
+    println!("cargo:rerun-if-changed={}", headers_path_str);
+
+    // Run `clang` to compile the `rgbcx.hpp` file into a `rgbcx.o` object file.
+    // Unwrap if it is not possible to spawn the process.
+    if !std::process::Command::new("clang")
+        .arg("-c")
+        .arg("-o")
+        .arg(&obj_path)
+        .arg(libdir_path.join("rgbcx.hpp"))
+        .output()
+        .expect("could not spawn `clang`")
+        .status
+        .success()
+    {
+        // Panic if the command was not successful.
+        panic!("could not compile object file");
+    }
+
+    // Run `ar` to generate the `librgbcx.a` file from the `rgbcx.o` file.
+    // Unwrap if it is not possible to spawn the process.
+    if !std::process::Command::new("ar")
+        .arg("rcs")
+        .arg(lib_path)
+        .arg(obj_path)
+        .output()
+        .expect("could not spawn `ar`")
+        .status
+        .success()
+    {
+        // Panic if the command was not successful.
+        panic!("could not emit library file");
+    }
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=rgbcx/rgbcx.hpp");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("rgbcx/rgbcx.hpp")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .allowlist_function(".*encode.*")
+        .allowlist_function(".*init")
+        .allowlist_item(".*AdvancedSettings.*")
+        .allowlist_item(".*ORDERINGS.*")
+        .allowlist_item(".*MIN_LEVEL.*")
+        .allowlist_item(".*MAX_LEVEL.*")
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
