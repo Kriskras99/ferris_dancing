40b0710f6cacfcb2633104a2a8e890e1749e14ea
Christiaan Biesterbosch <Kriskras99@users.noreply.github.com>
2024-03-22 16:53:32 +0100

    Fix sfattool and clippy warnings

--- dotstar_toolkit_utils/src/vfs/symlinkfs.rs
+++ dotstar_toolkit_utils/src/vfs/symlinkfs.rs
@@ -134,6 +134,6 @@ impl VirtualFileSystem for SymlinkFs<'_> {
     }
 
     fn exists(&self, path: &Path) -> bool {
-        self.mapping.get(path).is_some()
+        self.mapping.contains_key(path)
     }
 }
