a1bccd03487baa74da96b803cd2849be979d10f0
Christiaan Biesterbosch <Kriskras99@users.noreply.github.com>
2024-03-28 14:27:15 +0100

    Improve fuzzing setup

--- fuzz/fuzz_targets/act_write_and_parse.rs
+++ fuzz/fuzz_targets/act_write_and_parse.rs
@@ -4,12 +4,13 @@ extern crate libfuzzer_sys;
 
 use ubiart_toolkit::cooked::act::Actor;
 use libfuzzer_sys::fuzz_target;
+use dotstar_toolkit_utils::bytes::write::BinarySerialize;
+use ubiart_toolkit::utils::{Game, UniqueGameId, Platform};
 
 fuzz_target!(|actor: Actor| {
     let mut vec = Vec::new();
     if actor.serialize(&mut vec).is_ok() {
-        if let Ok(new_actor) = Actor::deserialize(&vec) {
-            assert_eq!(actor, new_actor)
-        }
+        let new_actor = ubiart_toolkit::cooked::act::parse(&vec, &mut 0, UniqueGameId { game: Game::JustDance2022, platform: Platform::Nx, id: 0}).unwrap();
+        assert_eq!(actor, new_actor);
     }
 });
