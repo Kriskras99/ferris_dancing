2dba6e6e1d9d4626c338b386946e85d97251bacf
Christiaan Biesterbosch <Kriskras99@users.noreply.github.com>
2024-03-28 09:06:55 +0100

    Start fixing act.ckd writer and add more UniqueGameIds

--- ubiart_toolkit/src/cooked/act/writer.rs
+++ ubiart_toolkit/src/cooked/act/writer.rs
@@ -13,19 +13,19 @@ impl BinarySerialize for Actor<'_> {
         writer: &mut (impl WriteAt + ?Sized),
         position: &mut u64,
     ) -> Result<(), WriteError> {
+        writer.write_at(position, &u32be::from(1))?; // unk0
         writer.write_at(position, &u32be::from(self.unk1))?;
         writer.write_at(position, &u32be::from(self.unk2))?;
         writer.write_at(position, &u32be::from(self.unk2_5))?;
-        writer.write_at(position, &u64be::from(0))?;
-        writer.write_at(position, &u32be::from(0))?;
-        writer.write_at(position, &u64be::from(0x1_0000_0000))?;
-        writer.write_at(position, &u32be::from(0))?;
-        writer.write_at(position, &u64be::from(0))?;
-        writer.write_at(position, &u64be::from(0xFFFF_FFFF))?;
-        writer.write_at(position, &u32be::from(0))?;
+        writer.write_at(position, &u64be::from(0))?; // unk3
+        writer.write_at(position, &u32be::from(0))?; // unk3_5
+        writer.write_at(position, &u64be::from(0x1_0000_0000))?; // unk4
+        writer.write_at(position, &u32be::from(0))?; // unk5
+        writer.write_at(position, &u64be::from(0))?; // unk6
+        writer.write_at(position, &u64be::from(0xFFFF_FFFF))?; // unk7
+        writer.write_at(position, &u32be::from(0))?; // unk8
         writer.write_at(position, &self.tpl)?;
-        writer.write_at(position, &u32be::from(0))?;
-        writer.write_at(position, &u32be::from(0))?;
+        writer.write_at(position, &u32be::from(0))?; // unk9
         writer.write_at(position, &u32be::try_from(self.components.len())?)?;
         for component in &self.components {
             writer.write_at(position, &u32be::from(component.to_id()))?;
@@ -69,14 +69,14 @@ fn write_material_graphic_component(
     is_pleo: bool,
 ) -> Result<(), WriteError> {
     for _ in 0..3 {
-        writer.write_at(position, &u32be::from(0x3F80_0000))?;
+        writer.write_at(position, &u32be::from(0x3F80_0000))?; // unk11
     }
     writer.write_at(position, &u32be::from(mgc.unk11_5))?;
     for _ in 0..2 {
         if is_pleo {
-            writer.write_at(position, &u64be::from(0xFFFF_FFFF))?;
+            writer.write_at(position, &u64be::from(0xFFFF_FFFF))?; // unk12
         } else {
-            writer.write_at(position, &u64be::from(0x0))?;
+            writer.write_at(position, &u64be::from(0x0))?; // unk12
         }
     }
     writer.write_at(position, &u32be::from(mgc.unk13))?;
